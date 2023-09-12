9d810b9423dd7039465ac90569510c0580700bbd
mayurisamanta <samantamayuri@gmail.com>
2023-09-12 16:34:26 +0530

    function in js

--- 02-js-refresher/code/finished/assets/scripts/app.js
+++ 02-js-refresher/code/finished/assets/scripts/app.js
@@ -8,22 +8,22 @@
 // console.log(util.default);
 // console.log(defaultkey);
 
-const userMessage = "Hello World!!!"; // variable cannot start with numbers and special characters (exception - $ _)
+// const userMessage = "Hello World!!!"; // variable cannot start with numbers and special characters (exception - $ _)
 // userMessage = "Mayuri"; // will give error because const is "read only"
 
-console.log(userMessage);
+// console.log(userMessage);
 
-// function createGreeting(userName, message = "Hello!") {
-// console.log(userName);
-// console.log(message);
-//   return "Hi, I am " + userName + ". " + message;
-// }
+function createGreeting(userName, message = "Hello!") {
+    console.log(userName);
+    console.log(message);
+    return "Hi, I am " + userName + ". " + message;
+}
 
-// const greeting1 = createGreeting("Max");
-// console.log(greeting1);
+let greeting1 = createGreeting("Max");
+console.log(greeting1);
 
-// const greeting2 = createGreeting("Manuel", "Hello, what's up?");
-// console.log(greeting2);
+const greeting2 = createGreeting("Manuel", "Hello, what's up?");
+console.log(greeting2);
 
 // export default (userName, message) => {
 //  console.log("Hello");
